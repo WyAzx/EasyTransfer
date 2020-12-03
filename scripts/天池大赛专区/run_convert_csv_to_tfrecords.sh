d3b5493d7c4fd344787088330ba86519232ec98d
v_yunzxwang <yunzxwang@qq.com>
2020-12-03 15:11:45 +0800

    add

--- scripts/天池大赛专区/run_convert_csv_to_tfrecords.sh
+++ scripts/天池大赛专区/run_convert_csv_to_tfrecords.sh
@@ -11,13 +11,13 @@ do
   python convert_csv_to_tfrecords.py --mode preprocess --config config/${TASK_NAME}_preprocess_dev.json
 done
 
-ls -d $PWD/tianchi_datasets/TNEWS/train.tfrecord > train.list_tfrecord
-ls -d $PWD/tianchi_datasets/OCEMOTION/train.tfrecord >> train.list_tfrecord
-ls -d $PWD/tianchi_datasets/OCNLI/train.tfrecord >> train.list_tfrecord
+ls -d tianchi_datasets/TNEWS/train.tfrecord > train.list_tfrecord
+ls -d tianchi_datasets/OCEMOTION/train.tfrecord >> train.list_tfrecord
+ls -d tianchi_datasets/OCNLI/train.tfrecord >> train.list_tfrecord
 
-ls -d $PWD/tianchi_datasets/TNEWS/dev.tfrecord > dev.list_tfrecord
-ls -d $PWD/tianchi_datasets/OCEMOTION/dev.tfrecord >> dev.list_tfrecord
-ls -d $PWD/tianchi_datasets/OCNLI/dev.tfrecord >> dev.list_tfrecord
+ls -d tianchi_datasets/TNEWS/dev.tfrecord > dev.list_tfrecord
+ls -d tianchi_datasets/OCEMOTION/dev.tfrecord >> dev.list_tfrecord
+ls -d tianchi_datasets/OCNLI/dev.tfrecord >> dev.list_tfrecord
 
 
 
